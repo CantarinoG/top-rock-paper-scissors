f170a151a2cd03fd8ff845c45dd722ed37193de1
CantarinoG <guilhermecantarino1709@gmail.com>
2022-10-22 22:24:19 -0300

    Added the main game function and invoke it

--- logic.js
+++ logic.js
@@ -18,5 +18,37 @@ function playRound(playerSelection, computerSelection) {
         return `You lose! ${com} beats ${p1}!`;
     } else if ((p1 == 'PAPER' && com == 'ROCK') || (p1 == 'SCISSORS' && com == 'PAPER') || (p1 == 'ROCK' && com == 'SCISSORS')) {
         return `You win! ${p1} beats ${com}!`;
+    } else {
+        return 'Oops! Something went wrong! Please try again!';
     }
 }
+
+function game() {
+    let userScore = 0;
+    let comScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let p1 = prompt("Choose a value: ROCK, PAPER or SCISSORS.");
+        let com = getComputerChoice();
+        let result = playRound(p1, com);
+        console.log(`Round ${i + 1}: ${result}`)
+        if (result.includes('win')) {
+            userScore++;
+        } else if (result.includes('lose')) {
+            comScore++;
+        }
+    }
+
+    if (userScore > comScore) {
+        console.log('You won the game!');
+    } else if (userScore < comScore) {
+        console.log('You lost the game!');
+    } else {
+        console.log('The game ended in a tie!');
+    }
+    console.log(`Score:
+    You: ${userScore}
+    Com: ${comScore}`);
+
+}
+
+game();
